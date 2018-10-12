118f80edc2ac28806d495bd2f62aa95f0d97b2d8
lscuquejo <leonardo.s@cuquejo.org>
2018-10-12 12:47:01 +0200

    Php and bootstrap

---
+++ study/6-php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <img src="img/back.jpg">
+
+  </body>
+</html>
+
+<?php
+
+echo "Hello sasda";
+
+?>
