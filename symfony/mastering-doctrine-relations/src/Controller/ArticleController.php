ca2d45fd13d5a8bc4eb1d5193a7f8b2c2e59278f
lscuquejo <leonardo.s@cuquejo.org>
2019-02-13 18:15:54 +0100

    symfony fundamentals 1

--- symfony/mastering-doctrine-relations/src/Controller/ArticleController.php
+++ symfony/mastering-doctrine-relations/src/Controller/ArticleController.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\CommentRepository;
 use App\Service\MarkdownHelper;
 use App\Service\SlackClient;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,52 +12,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
-
 class ArticleController extends AbstractController
 {
-    /**
-     * Currently unused: just showing a controller with a constructor!
-     */
     private $isDebug;
-
     public function __construct(bool $isDebug)
     {
         $this->isDebug = $isDebug;
     }
-
     /**
      * @Route("/", name="app_homepage")
      */
     public function homepage(ArticleRepository $repository)
     {
         $articles = $repository->findAllPublishedOrderedByNewest();
-
         return $this->render('article/homepage.html.twig', [
             'articles' => $articles,
         ]);
     }
-
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show(Article $article, SlackClient $slack)
+    public function show(Article $article, SlackClient $slack, CommentRepository $commentRepository)
     {
         if ($article->getSlug() === 'khaaaaaan') {
             $slack->sendMessage('Kahn', 'Ah, Kirk, my old friend...');
         }
-
+        $comments = $commentRepository->findBy(['article' => $article]);
+        dump($comments);die;
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
-
         return $this->render('article/show.html.twig', [
             'article' => $article,
             'comments' => $comments,
         ]);
     }
-
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
@@ -66,9 +56,7 @@ class ArticleController extends AbstractController
     {
         $article->incrementHeartCount();
         $em->flush();
-
         $logger->info('Article is being hearted!');
-
         return new JsonResponse(['hearts' => $article->getHeartCount()]);
     }
 }
